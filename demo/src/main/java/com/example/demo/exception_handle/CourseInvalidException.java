3b4fbc7f1387c12994ee525d7947067aad79a9be
Ngan cute hme <ngannguyenqpna@gmail.com>
2024-11-03 22:48:02 +0700

    add: home

--- demo/src/main/java/com/example/demo/exception_handle/CourseInvalidException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception_handle;
-
-public class CourseInvalidException extends RuntimeException{
-    private String message;
-    public CourseInvalidException(String message) {
-        super(message);
-    }
-}
