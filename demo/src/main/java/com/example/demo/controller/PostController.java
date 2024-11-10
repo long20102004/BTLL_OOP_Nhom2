7059c25d6ad16cf18ae9420366f6779449043083
long20102004 <hoanghailonguyno@gmail.com>
2024-11-10 16:24:09 +0700

    alo

--- demo/src/main/java/com/example/demo/controller/PostController.java
+++ demo/src/main/java/com/example/demo/controller/PostController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.PostEntity;
 import com.example.demo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
--- demo/src/main/java/com/example/demo/repository/PostRepository.java
+++ demo/src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<PostEntity, Integer>{
